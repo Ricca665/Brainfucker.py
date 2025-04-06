afe84020d9197674b3d60f8ec038799c0cf662c1
Ricca665 <riccardosarti2010@gmail.com>
2025-04-06 18:35:02 +0200

    Added finishing touches

--- src/main.py
+++ src/main.py
@@ -18,12 +18,28 @@ memory = [0] * 30000 # 30000 memory cells
 pointer = 0
 
 memory[pointer] += 1
+pc = 0 # Just used in loops
+
+temp_stack = []
+bracemap = {}
 
 brainfuck_program = open(args.brainfuck_program_name, "r")
 brainfuck_program = brainfuck_program.read()
 
+"""We need to check for each [ it's maching ]"""
+for pos, char in enumerate(brainfuck_program):
+    if char == "[":
+        temp_stack.append(pos)
+    elif char == "]":
+        start = temp_stack.pop()
+        bracemap[start] = pos
+        bracemap[pos] = start
+
 try:
-    for i in brainfuck_program:
+    while pc < len(brainfuck_program): # While program counter is less than the lenght of the program
+        i = brainfuck_program[pc] # store the current instruction in i
+
+        """Evaluation of each instructions"""
         if i == "+":
             memory[pointer] += 1
         elif i == "-":
@@ -43,13 +59,18 @@ try:
             except Exception:
                 pass
         elif i == "[":
+            if memory[pointer] == 0:
+                pc = bracemap[pc]  # Jump to matching "]" if current cell is 0
+        elif i == "]":
             if memory[pointer] != 0:
-                continue
-        else:
-            continue
-        if args.verbose:
+                pc = bracemap[pc]  # Jump to matching "[" if current cell is non-zero
+
+        if args.verbose: #Debug
             logging.debug(memory[pointer])
-            
-except IndexError:
-    pointer -= 1
-    
+        
+        pc += 1
+
+except IndexError: # In case the fucking pointer exits
+    pointer -= 1 # Reduce our pointer
+except Exception: # Ignore any python exception
+    pass
