d41501ace01f2adf588b46c60950cf8c0ba2b295
Ricca665 <riccardosarti2010@gmail.com>
2025-04-10 21:07:29 +0200

    Commented the code more

--- src/interpreter.py
+++ src/interpreter.py
@@ -4,25 +4,28 @@ import argparse
 from re import search
 import sys
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG) # Sets our logging level
 
 def main(args, memory, pointer):
     try:
-        brainfuck_program = open(args.brainfuck_program_name, "r")     
-    except FileNotFoundError:
+        brainfuck_program = open(args.brainfuck_program_name, "r") # Opens the file
+    except FileNotFoundError: # In case the file doesn't exist
         print("ERROR: File not found!")
         sys.exit(1)
 
-    brainfuck_program = brainfuck_program.read()
-    run_program(brainfuck_program, memory, pointer)
+    brainfuck_program = brainfuck_program.read() # We just read the file
+
+    run_program(brainfuck_program, memory, pointer) # We run the program
+
+    brainfuck_program.close() # We close the file just in case
 
 def run_program(brainfuck_program, memory, pointer):
-    """Runs the brainfuck program"""
+    # Main interpreter
     pc = 0 # Program counter
     temp_stack = []
     bracemap = {}
 
-    """We need to check for each [ it's maching ]"""
+    #We need to check for each [ it's maching ]
     for pos, char in enumerate(brainfuck_program):
         if char == "[":
             temp_stack.append(pos)
@@ -35,27 +38,32 @@ def run_program(brainfuck_program, memory, pointer):
         while pc < len(brainfuck_program): # While program counter is less than the lenght of the program
             i = brainfuck_program[pc] # store the current instruction in i
 
-            """Evaluation of each instructions"""
+            #Evaluation of each instructions
+
             if i == "+":
-                memory[pointer] += 1
+                memory[pointer] += 1 # Increases current mem cell by one
             elif i == "-":
-                memory[pointer] -= 1
-            elif i == ">":
+                memory[pointer] -= 1 # Decreases current mem cell by one
+            elif i == ">": # Moves the pointer, changing the cell
                 if pointer < 30000:
                     pointer += 1
-            elif i == "<":
+            elif i == "<": # Moves the pointer like before, but backwards
                 if pointer > 0:
                     pointer -= 1
             elif i == ".":
                 print(chr(memory[pointer])) # Converts the current cell to ascii and prints it
             elif i == "_":
                 print(memory[pointer]) # Like the . but it doesn't convert the number to ascii, useful for debugging
-            elif i == ",":
-                value = ord(str(input()[0]))
+
+            elif i == ",": # Takes input in
                 try:
+                    value = ord(str(input()[0]))
                     memory[pointer] = value
-                except Exception:
-                    pass
+                except Exception: # Prevent python exception
+                    if args.verbose:
+                        logging.debug("An error has occured while trying to take the input")
+            
+            # Most of the loop code taken from: https://github.com/pocmo/Python-Brainfuck
             elif i == "[":
                 if memory[pointer] == 0:
                     pc = bracemap[pc]  # Jump to matching "]" if current cell is 0
@@ -73,7 +81,7 @@ def run_program(brainfuck_program, memory, pointer):
                     pc = len(url)-1
                     url_code = url_code.status_code # Gets the status code
                     memory[pointer] = url_code # Puts the status code in the current memory pointer
-                else:
+                else: # Prints error if no url was found, but just when debugging is enabled
                     if args.verbose:
                         logging.debug("Error while searching for link")
 
@@ -85,9 +93,7 @@ def run_program(brainfuck_program, memory, pointer):
                     pc = len(url)-1
                     url_code = url_code.status_code # Gets the status code
                     memory[pointer] = url_code # Puts the status code in the current memory pointer
-                    logging.debug(url_code)
-                    logging.debug(url)
-                else:
+                else: # Prints error if no url was found, but just when debugging is enabled
                     if args.verbose:
                         logging.debug("Error while searching for link")
 
@@ -97,14 +103,16 @@ def run_program(brainfuck_program, memory, pointer):
             pc += 1
 
 
-    except IndexError: # In case the fucking pointer exits
+    except IndexError: # In case the fucking pointer exits out of the fucking boundary
         pointer -= 1 # Reduce our pointer
     except Exception as e: # Ignore any python exception
-        if args.verbose:
-            logging.debugging(f"The unexpected error that wasn't expected was expected: {e}")
+        if args.verbose: # Make sure that debugging is enabled
+            logging.debugging(f"The unexpected error that wasn't expected was expected: {e}") # Print error
 
 
 if __name__ == "__main__":
+
+    # We specify the args, what they do, and parse them in the variable "args"
     parser = argparse.ArgumentParser(
                         prog='Brainfucker.py++',
                         description='Brainfuck interpreter for python, with extra feaures',
@@ -116,9 +124,8 @@ if __name__ == "__main__":
     parser.add_argument('-b', '--blocks')
     args = parser.parse_args()
 
-
-    memory = [0] * 30000 # 30000 memory cells
-    pointer = 0
+    memory = [0] * 30000 # 30000 memory cells # Creates the memory cells
+    pointer = 0 # Sets our pointer
 
     if args.blocks:
         memory = None
@@ -126,7 +133,9 @@ if __name__ == "__main__":
             memory = [0]*int(args.blocks)
         except Exception: # In case it's invalid
             memory = [0]*30000 # Set it to default 
-    if not args.verbose:
+
+    if not args.verbose: # Disable requests debug messages if not in debug mode
         logging.getLogger("urllib3").setLevel(logging.WARNING)
 
-    main(args, memory, pointer)
+    main(args, memory, pointer) # Run NOT the main program, but just the thing to open our file
+    # :3
